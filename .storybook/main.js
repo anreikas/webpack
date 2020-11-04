f7f01f64add3a5c374ea5d19fe94e473a425f403
anreikas <kazeyandrey@gmail.com>
2020-11-04 11:24:39 +0300

    fix: stories path

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/js/components/**/*.stories.mdx",
+    "../src/js/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
