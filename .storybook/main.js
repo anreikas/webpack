7829d1dc8b32f5249e9ceb320072fd0309c50f1a
anreikas <kazeyandrey@gmail.com>
2020-11-04 13:02:42 +0300

    feat: add scss support for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,12 @@
 module.exports = {
-  "stories": [
-    "../src/js/components/**/*.stories.mdx",
-    "../src/js/components/**/*.stories.@(js|jsx|ts|tsx)"
+  'stories': [
+    '../src/js/components/**/*.stories.mdx',
+    '../src/js/components/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  'addons': [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-scss'
   ]
 }
+
